b0f8f5268610a53d3d02572af15b100a28e4be18
sonrisahy <sonrisa@cap.ocn.ne.jp>
2023-04-06 05:26:27 +0900

    20230406 update

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
+import {useRouter} from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
-import styles from '../styles/Layout.module.css'
+import Showcase from './Showcase'
+import styles from '@/styles/Layout.module.css'
 
 function Layout({title, keywords, description, children}) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -14,6 +18,9 @@ function Layout({title, keywords, description, children}) {
         {keywords} />
       </Head>
       <Header />
+
+      {router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>
       {children}
       </div>
